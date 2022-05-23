f1502bbc0d5f3ae4499f6bd8922e2fbff0064ba3
shaikfaruk <shaikfaruk04@gmail.com>
2022-05-23 14:12:12 +0530

    secondpush

---
+++ src/Programe/Sumofarray.java
@@ -0,0 +1,17 @@
+package Programe;
+
+public class Sumofarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int a[]={20,40,40,20,30};
+          int sum=0;
+          for(int i=0;i<=a.length;i++){
+        	  sum=sum+a[i];
+        	  
+          }
+          System.out.println(sum);
+          
+	}
+
+}
